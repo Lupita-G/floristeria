784844c3729c74ea4c835beeb7f17af8577899ee
Lupita-G <lupitagm390@gmail.com>
2020-12-11 18:40:46 -0600

    Agregando Js validando formulario

---
+++ ocasiones.js
@@ -0,0 +1,34 @@
+(function (){
+    let formulario = document.getElementById('formulario'),
+        elementos = formulario.elements,
+        boton = document.getElementById('btn');
+
+    let validarNombre = function(e){
+        if(formulario.nombre.value == 0) {
+            alert("Completa el campo nombre");
+            e.preventDefault();
+        }
+    };
+
+    let validarEmail = function(e){
+        if(formulario.email.value == 0) {
+            alert("Ingrese un email para continuar");
+            e.preventDefault();
+        }
+    };
+
+    let validarCheckbox = function(e){
+        if(formulario.terminos.checked == false) {
+            alert("Acepta los Terminos");
+            e.preventDefault();
+        }
+    }
+
+    let validar = function(e){
+        validarNombre(e);
+        validarEmail(e);
+        validarCheckbox(e);
+    };
+
+    formulario.addEventListener("submit", validar);
+}())
